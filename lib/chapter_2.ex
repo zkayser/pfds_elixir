0737a881fcbf7629cd02570e2410ab4789aa7e89
Kayser <zkayser@gmail.com>
2019-03-05 08:14:39 -0500

    Chapter 2 exercises

--- lib/chapter_2.ex
+++ lib/chapter_2.ex
@@ -1,4 +1,8 @@
 defmodule PFDS.Chapter2 do
+  @type el() :: term()
+  @opaque tree() :: :empty | {tree, el, tree}
+
+
   @spec suffixes(list(any())) :: list(list(any()))
   def suffixes(list) do
     do_suffixes(list, [])
@@ -12,13 +16,11 @@ defmodule PFDS.Chapter2 do
 
   defmodule UnbalancedSet do
     alias PFDS.Ordered
+    alias PFDS.Chapter2
 
-    @type el() :: term()
-    @opaque tree() :: :empty | {tree, el, tree}
-    @type set() :: tree
-
-    @spec member?(el, tree()) :: bool()
+    @spec member?(Chapter2.el, Chapter2.tree()) :: bool()
     def member?(_, :empty), do: false
+
     def member?(el, {left, root, right}) do
       cond do
         Ordered.lt(el, root) -> member?(el, left)
@@ -41,7 +43,7 @@ defmodule PFDS.Chapter2 do
     it significantly more performant than the naive implementation of `member?/2` above while maintaining its
     behavior.
     """
-    @spec efficient_member(el, tree()) :: bool()
+    @spec efficient_member(Chapter2.el, Chapter2.tree()) :: bool()
     def efficient_member(el, tree) do
       case tree do
         :empty -> false
@@ -51,11 +53,15 @@ defmodule PFDS.Chapter2 do
     end
 
     defp handle_member(el, :empty, candidate), do: el == candidate
-    defp handle_member(el, {left, root, _}, candidate) when el < root, do: handle_member(el, left, candidate)
+
+    defp handle_member(el, {left, root, _}, candidate) when el < root,
+      do: handle_member(el, left, candidate)
+
     defp handle_member(el, {_, root, right}, _), do: handle_member(el, right, root)
 
-    @spec insert(el, tree) :: tree()
+    @spec insert(Chapter2.el, Chapter2.tree()) :: Chapter2.tree()
     def insert(el, :empty), do: {:empty, el, :empty}
+
     def insert(el, {left, root, right} = tree) do
       cond do
         Ordered.lt(el, root) -> {insert(el, left), root, right}
@@ -63,5 +69,107 @@ defmodule PFDS.Chapter2 do
         true -> tree
       end
     end
+
+    defmodule ExistingElementException do
+      defexception [:message]
+
+      @impl true
+      def exception(element) do
+        %ExistingElementException{
+          message: "Element #{inspect(element, pretty: true)} already exists"
+        }
+      end
+    end
+
+    @doc """
+    Exercise 2.3
+    Inserting an existing element into a binary search tree copies the entire search path even though the
+    copied nodes are indistinguishable from the originals. Rewrite `insert` using exceptions to avoid this copying.
+    Establish one handler per insertion rather than per iteration.
+    """
+    @spec efficient_insert(Chapter2.el, Chapter2.tree()) :: {:ok, Chapter2.tree()} | {:existing_element, Chapter2.tree()}
+    def efficient_insert(el, tree) do
+      try do
+        {:ok, handle_efficient_insert(el, tree)}
+      rescue
+        ExistingElementException -> {:existing_element, tree}
+      end
+    end
+
+    defp handle_efficient_insert(el, :empty), do: {:empty, el, :empty}
+
+    defp handle_efficient_insert(el, {_, root, _}) when el == root do
+      raise(ExistingElementException, el)
+    end
+
+    defp handle_efficient_insert(el, {left, root, right}) do
+      case Ordered.lt(el, root) do
+        true -> {handle_efficient_insert(el, left), root, right}
+        false -> {left, root, handle_efficient_insert(el, right)}
+      end
+    end
+
+    @doc """
+    Exercise 2.4
+    Combine the ideas of the previous two exercises to obtain a version
+    of insert that performs no unnecessary copying and uses no more than
+    d + 1 comparisons
+    """
+    @spec optimized_insert(Chapter2.el, Chapter2.tree()) :: Chapter2.tree()
+    def optimized_insert(el, tree), do: optimized_insert_(el, nil, tree)
+    defp optimized_insert_(el, el, :empty), do: raise(ExistingElementException, el)
+    defp optimized_insert_(el, _prev, :empty), do: {:empty, el, :empty}
+
+    defp optimized_insert_(el, previous, {left, root, right}) when el < root do
+      {optimized_insert_(el, previous, left), root, right}
+    end
+
+    defp optimized_insert_(el, _, {left, root, right}) do
+      {left, root, optimized_insert_(el, root, right)}
+    end
+  end
+
+  @doc """
+  Exercise 2.5 A)
+  Sharing can be also be useful within a single object, not just between objects. For example,
+  if the two subtrees of a given node are identical, then they can be represented by the same tree.
+
+  a) Using this idea, write a function `complete` of type `Elem -> Int -> Tree` where `complete(x, d)`
+  creates a binary tree of depth `d` with `x` stored in every node. (Of course, this function makes no
+  sense for the set abstraction, but it can be a useful auxilary function for other abstractions, such
+  as bags.) This function should run in `O(d)` time.
+  """
+  @spec complete(el, non_neg_integer()) :: tree()
+  def complete(_x, 0), do: :empty
+  def complete(x, d) do
+    sub_tree = complete(x, d - 1)
+    { sub_tree, x, sub_tree }
+  end
+
+  @doc """
+  Exercise 2.5 B)
+
+  Extend the above `complete` function to create balanced trees of arbitrary size. These trees will
+  not always be complete binary trees, but should be as balanced as possible: for any given node,
+  the two subtrees should differ in size by at most one. This function should run in `O(log n)` time.
+  """
+  def balanced(_x, 0), do: :empty
+  def balanced(x, size) do
+    {sub_tree_1, sub_tree_2} = create_2(x, div(size - 1, 2))
+    case rem(size, 2) == 0 do
+      true -> { sub_tree_1, x, sub_tree_2 }
+      false -> { sub_tree_1, x, sub_tree_1 }
+    end
+  end
+
+  defp create_2(_x, size) when size < 0, do: {:empty, :empty}
+  defp create_2(x, 0), do: {:empty, {:empty, x, :empty}}
+  defp create_2(x, size) when rem(size, 2) != 0 do
+    { sub_tree_1, sub_tree_2 } = create_2(x, div(size - 1, 2))
+    { { sub_tree_1, x, sub_tree_1}, { sub_tree_1, x, sub_tree_2 } }
+  end
+  defp create_2(x, size) do
+    { sub_tree_1, sub_tree_2 } = create_2(x, div(size - 2, 2))
+    { {sub_tree_1, x, sub_tree_2}, {sub_tree_2, x, sub_tree_2} }
   end
 end
--- test/chapter_2_test.exs
+++ test/chapter_2_test.exs
@@ -3,10 +3,10 @@ defmodule PFDS.Chapter2Test do
   import PFDS.Chapter2
   alias PFDS.Chapter2.UnbalancedSet
 
-  @left_sub_tree { :empty, 4, :empty }
+  @left_sub_tree {:empty, 4, :empty}
   @root_node 5
-  @right_sub_tree { :empty, 6, { :empty, 7, { :empty, 8, :empty } } }
-  @a_tree { @left_sub_tree, @root_node, @right_sub_tree }
+  @right_sub_tree {:empty, 6, {:empty, 7, {:empty, 8, :empty}}}
+  @a_tree {@left_sub_tree, @root_node, @right_sub_tree}
 
   describe "suffixes/1" do
     test "returns a list of all the suffixes of xs in descending order of length" do
@@ -31,11 +31,11 @@ defmodule PFDS.Chapter2Test do
 
   describe "insert/2" do
     test "places the element to be inserted in the left sub tree if smaller than the root node" do
-      assert { { :empty, 1, :empty }, 4, :empty } = UnbalancedSet.insert(1, @left_sub_tree)
+      assert {{:empty, 1, :empty}, 4, :empty} = UnbalancedSet.insert(1, @left_sub_tree)
     end
 
     test "places the element to be inserted in the right sub tree if larger than the root node" do
-      assert { :empty, 4, { :empty, 5, :empty }} = UnbalancedSet.insert(5, @left_sub_tree)
+      assert {:empty, 4, {:empty, 5, :empty}} = UnbalancedSet.insert(5, @left_sub_tree)
     end
   end
 
@@ -51,6 +51,51 @@ defmodule PFDS.Chapter2Test do
       refute UnbalancedSet.efficient_member(1, @a_tree)
       refute UnbalancedSet.efficient_member(0, @a_tree)
       refute UnbalancedSet.efficient_member(100, @a_tree)
+    end
+  end
+
+  describe "efficient_insert/2" do
+    test "returns {:error, :existing_element, original_tree} when inserting an existing element" do
+      assert {:existing_element, @a_tree} = UnbalancedSet.efficient_insert(7, @a_tree)
+    end
+
+    test "return {:ok, {:empty, element, :empty} when inserting into an empty tree" do
+      assert {:ok, {:empty, 2, :empty}} = UnbalancedSet.efficient_insert(2, :empty)
+    end
+  end
+
+  describe "optimized_insert/2" do
+    test "places the element to be inserted in the left sub tree if smaller than the root node" do
+      assert {{:empty, 1, :empty}, 4, :empty} = UnbalancedSet.optimized_insert(1, @left_sub_tree)
+    end
+
+    test "places the element to be inserted in the right sub tree if larger than the root node" do
+      assert {:empty, 4, {:empty, 5, :empty}} = UnbalancedSet.optimized_insert(5, @left_sub_tree)
+    end
+
+    test "raises ExistingElementException when inserting an existing element" do
+      assert_raise(PFDS.Chapter2.UnbalancedSet.ExistingElementException, fn ->
+        UnbalancedSet.optimized_insert(6, @a_tree)
+      end)
+    end
+  end
+
+  describe "complete/2" do
+    test "creates a complete binary tree of depth d with element x stored in every node" do
+      {x, d} = {1, 4}
+      assert {left, 1, right} = complete(x, d)
+      assert left == right
+      assert { level_2_left, 1, level_2_right } = left
+      assert { level_3_left, 1, level_3_right } = level_2_left
+      assert { level_4_left, 1, level_4_right } = level_3_left
+      assert :empty = level_4_left
+    end
+  end
+
+  describe "create/2" do
+    test "creates balanced trees of arbitrary size, s" do
+      assert {{:empty, 1, :empty}, 1, {:empty, 1, :empty}} == balanced(1, 3)
+      assert {:empty, 1, {:empty, 1, :empty}} == balanced(1, 2)
     end
   end
 end
