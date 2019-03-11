e90f2796a9dce0e6931d54a7a0aa511b75dcaa74
Kayser <zkayser@gmail.com>
2019-03-11 08:35:54 -0400

    Implement enumerable protocol for UnbalancedSet

--- lib/chapter_2/unbalanced_set.ex
+++ lib/chapter_2/unbalanced_set.ex
@@ -2,13 +2,33 @@ defmodule UnbalancedSet do
   alias PFDS.Ordered
   alias PFDS.Chapter2
 
-  @spec member?(Chapter2.el(), Chapter2.tree()) :: bool()
-  def member?(_, :empty), do: false
+  defstruct set: :empty
+  @type t() :: %__MODULE__{set: Chapter2.tree()}
 
-  def member?(el, {left, root, right}) do
+  @doc """
+  Creates a new empty UnbalancedSet
+  """
+  @spec new() :: t()
+  def new(), do: %__MODULE__{}
+
+  @doc """
+  Creates an UnbalancedSet from a list
+  """
+  def from_list([]), do: %__MODULE__{}
+  def from_list(list) do
+    list
+    |> Enum.reduce(%__MODULE__{}, fn x, acc ->
+      optimized_insert(x, acc)
+    end)
+  end
+
+  @spec member?(Chapter2.el(), __MODULE__.t()) :: bool()
+  def member?(_, %__MODULE__{set: :empty}), do: false
+
+  def member?(el, %__MODULE__{set: {left, root, right}}) do
     cond do
-      Ordered.lt(el, root) -> member?(el, left)
-      Ordered.lt(root, el) -> member?(el, right)
+      Ordered.lt(el, root) -> member?(el, %UnbalancedSet{set: left})
+      Ordered.lt(root, el) -> member?(el, %UnbalancedSet{set: right})
       true -> true
     end
   end
@@ -27,8 +47,8 @@ defmodule UnbalancedSet do
   it significantly more performant than the naive implementation of `member?/2` above while maintaining its
   behavior.
   """
-  @spec efficient_member(Chapter2.el(), Chapter2.tree()) :: bool()
-  def efficient_member(el, tree) do
+  @spec efficient_member(Chapter2.el(), __MODULE__.t()) :: bool()
+  def efficient_member(el, %__MODULE__{set: tree}) do
     case tree do
       :empty -> false
       {left, root, _} when el < root -> handle_member(el, left, nil)
@@ -43,13 +63,15 @@ defmodule UnbalancedSet do
 
   defp handle_member(el, {_, root, right}, _), do: handle_member(el, right, root)
 
-  @spec insert(Chapter2.el(), Chapter2.tree()) :: Chapter2.tree()
-  def insert(el, :empty), do: {:empty, el, :empty}
+  @spec insert(Chapter2.el(), __MODULE__.t()) :: __MODULE__.t()
+  def insert(el, :empty), do: %__MODULE__{set: {:empty, el, :empty}}
+  def insert(el, %__MODULE__{set: set}), do: %__MODULE__{set: insert_(el, set)}
 
-  def insert(el, {left, root, right} = tree) do
+  defp insert_(el, :empty), do: {:empty, el, :empty}
+  defp insert_(el, {left, root, right} = tree) do
     cond do
-      Ordered.lt(el, root) -> {insert(el, left), root, right}
-      Ordered.lt(root, el) -> {left, root, insert(el, right)}
+      Ordered.lt(el, root) -> {insert_(el, left), root, right}
+      Ordered.lt(root, el) -> {left, root, insert_(el, right)}
       true -> tree
     end
   end
@@ -73,9 +95,9 @@ defmodule UnbalancedSet do
   """
   @spec efficient_insert(Chapter2.el(), Chapter2.tree()) ::
           {:ok, Chapter2.tree()} | {:existing_element, Chapter2.tree()}
-  def efficient_insert(el, tree) do
+  def efficient_insert(el, %__MODULE__{set: tree}) do
     try do
-      {:ok, handle_efficient_insert(el, tree)}
+      {:ok, %UnbalancedSet{set: handle_efficient_insert(el, tree)}}
     rescue
       ExistingElementException -> {:existing_element, tree}
     end
@@ -100,8 +122,8 @@ defmodule UnbalancedSet do
   of insert that performs no unnecessary copying and uses no more than
   d + 1 comparisons
   """
-  @spec optimized_insert(Chapter2.el(), Chapter2.tree()) :: Chapter2.tree()
-  def optimized_insert(el, tree), do: optimized_insert_(el, nil, tree)
+  @spec optimized_insert(Chapter2.el(), t()) :: t()
+  def optimized_insert(el, %__MODULE__{set: tree}), do: %__MODULE__{set: optimized_insert_(el, nil, tree)}
   defp optimized_insert_(el, el, :empty), do: raise(ExistingElementException, el)
   defp optimized_insert_(el, _prev, :empty), do: {:empty, el, :empty}
 
@@ -112,4 +134,32 @@ defmodule UnbalancedSet do
   defp optimized_insert_(el, _, {left, root, right}) do
     {left, root, optimized_insert_(el, root, right)}
   end
+end
+
+defimpl Enumerable, for: UnbalancedSet do
+  @impl true
+  def reduce(_set, {:halt, acc}, _fun), do: {:halted, acc}
+  def reduce(set, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(set, &1, fun)}
+  def reduce(%UnbalancedSet{set: :empty}, {:cont, acc}, _fun), do: {:done, acc}
+  def reduce(%UnbalancedSet{set: set}, {:cont, acc}, fun) do
+    {:done,  reduce_(set, acc, fun)}
+  end
+  def reduce(set, tagged_acc, fun), do: reduce(%UnbalancedSet{set: set}, tagged_acc, fun)
+
+  defp reduce_(:empty, acc, _fun), do: acc
+  defp reduce_({left, root, right}, acc, fun) do
+    {:cont, intermediate} = fun.(acc, root)
+    {:cont, sum} = fun.(reduce_(left, acc, fun), reduce_(right, acc, fun))
+    {:cont, res} = fun.(intermediate, sum)
+    res
+  end
+
+  @impl true
+  def count(_), do: {:error, __MODULE__}
+
+  @impl true
+  def member?(set, el), do: {:ok, UnbalancedSet.efficient_member(el, set)}
+
+  @impl true
+  def slice(_set), do: {:error, __MODULE__}
 end
--- test/chapter_2/unbalanced_set_test.exs
+++ test/chapter_2/unbalanced_set_test.exs
@@ -5,70 +5,103 @@ defmodule UnbalancedSetTest do
   @root_node 5
   @right_sub_tree {:empty, 6, {:empty, 7, {:empty, 8, :empty}}}
   @a_tree {@left_sub_tree, @root_node, @right_sub_tree}
+  @set %UnbalancedSet{set: @a_tree}
 
   describe "member?/2" do
     test "returns true if the query element is in the tree" do
-      assert UnbalancedSet.member?(5, @a_tree)
-      assert UnbalancedSet.member?(4, @a_tree)
-      assert UnbalancedSet.member?(6, @a_tree)
-      assert UnbalancedSet.member?(7, @a_tree)
+      assert UnbalancedSet.member?(5, @set)
+      assert UnbalancedSet.member?(4, @set)
+      assert UnbalancedSet.member?(6, @set)
+      assert UnbalancedSet.member?(7, @set)
     end
 
     test "returns false if the query element is not in the tree" do
-      refute UnbalancedSet.member?(1, @a_tree)
-      refute UnbalancedSet.member?(0, @a_tree)
-      refute UnbalancedSet.member?(100, @a_tree)
+      refute UnbalancedSet.member?(1, @set)
+      refute UnbalancedSet.member?(0, @set)
+      refute UnbalancedSet.member?(100, @set)
     end
   end
 
   describe "insert/2" do
     test "places the element to be inserted in the left sub tree if smaller than the root node" do
-      assert {{:empty, 1, :empty}, 4, :empty} = UnbalancedSet.insert(1, @left_sub_tree)
+      assert %UnbalancedSet{set: {{:empty, 1, :empty}, 4, :empty}} = UnbalancedSet.insert(1, %UnbalancedSet{set: @left_sub_tree})
     end
 
     test "places the element to be inserted in the right sub tree if larger than the root node" do
-      assert {:empty, 4, {:empty, 5, :empty}} = UnbalancedSet.insert(5, @left_sub_tree)
+      assert %UnbalancedSet{set: {:empty, 4, {:empty, 5, :empty}}} = UnbalancedSet.insert(5, %UnbalancedSet{set: @left_sub_tree})
     end
   end
 
   describe "efficient_member/2" do
     test "returns true if the query element is in the tree" do
-      assert UnbalancedSet.efficient_member(5, @a_tree)
-      assert UnbalancedSet.efficient_member(4, @a_tree)
-      assert UnbalancedSet.efficient_member(6, @a_tree)
-      assert UnbalancedSet.efficient_member(7, @a_tree)
+      assert UnbalancedSet.efficient_member(5, @set)
+      assert UnbalancedSet.efficient_member(4, @set)
+      assert UnbalancedSet.efficient_member(6, @set)
+      assert UnbalancedSet.efficient_member(7, @set)
     end
 
     test "returns false if the query element is not in the tree" do
-      refute UnbalancedSet.efficient_member(1, @a_tree)
-      refute UnbalancedSet.efficient_member(0, @a_tree)
-      refute UnbalancedSet.efficient_member(100, @a_tree)
+      refute UnbalancedSet.efficient_member(1, @set)
+      refute UnbalancedSet.efficient_member(0, @set)
+      refute UnbalancedSet.efficient_member(100, @set)
     end
   end
 
   describe "efficient_insert/2" do
     test "returns {:error, :existing_element, original_tree} when inserting an existing element" do
-      assert {:existing_element, @a_tree} = UnbalancedSet.efficient_insert(7, @a_tree)
+      assert {:existing_element, @a_tree} = UnbalancedSet.efficient_insert(7, @set)
     end
 
     test "return {:ok, {:empty, element, :empty} when inserting into an empty tree" do
-      assert {:ok, {:empty, 2, :empty}} = UnbalancedSet.efficient_insert(2, :empty)
+      assert {:ok, %UnbalancedSet{set: {:empty, 2, :empty}}} = UnbalancedSet.efficient_insert(2, %UnbalancedSet{set: :empty})
     end
   end
 
   describe "optimized_insert/2" do
     test "places the element to be inserted in the left sub tree if smaller than the root node" do
-      assert {{:empty, 1, :empty}, 4, :empty} = UnbalancedSet.optimized_insert(1, @left_sub_tree)
+      assert %UnbalancedSet{set: {{:empty, 1, :empty}, 4, :empty}} = UnbalancedSet.optimized_insert(1, %UnbalancedSet{set: @left_sub_tree})
     end
 
     test "places the element to be inserted in the right sub tree if larger than the root node" do
-      assert {:empty, 4, {:empty, 5, :empty}} = UnbalancedSet.optimized_insert(5, @left_sub_tree)
+      assert %UnbalancedSet{set: {:empty, 4, {:empty, 5, :empty}}} = UnbalancedSet.optimized_insert(5, %UnbalancedSet{set: @left_sub_tree})
     end
 
     test "raises ExistingElementException when inserting an existing element" do
       assert_raise(UnbalancedSet.ExistingElementException, fn ->
-        UnbalancedSet.optimized_insert(6, @a_tree)
+        UnbalancedSet.optimized_insert(6, @set)
       end)
+    end
+  end
+
+  describe "new/0" do
+    test "creates a new empty set" do
+      assert UnbalancedSet.new() == %UnbalancedSet{set: :empty}
+    end
+  end
+
+  describe "from_list/1" do
+    test "creates an unbalanced set from an Elixir list" do
+      assert UnbalancedSet.from_list([]) == %UnbalancedSet{set: :empty}
+      assert UnbalancedSet.from_list([1]) == %UnbalancedSet{set: {:empty, 1, :empty}}
+      assert UnbalancedSet.from_list([5, 4, 6, 7, 8]) ==
+        %UnbalancedSet{set: @a_tree}
+    end
+  end
+
+  describe "Enumerable.UnbalancedSet" do
+    test "allows you to call functions from the Enum module" do
+      assert 30 ==
+        [7, 5, 6, 4, 8]
+        |> UnbalancedSet.from_list()
+        |> Enum.reduce(0, &Kernel.+/2)
+    end
+
+    test "reduces over larger sets" do
+      range = 1..200
+      assert Enum.reduce(range, 0, &Kernel.+/2) ==
+        range
+        |> UnbalancedSet.from_list()
+        |> Enum.reduce(0, &Kernel.+/2)
     end
   end
 end
