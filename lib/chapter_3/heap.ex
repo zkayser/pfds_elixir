5ece74ec8a7c45a86c1c0183049cabc9f798809b
Kayser <zkayser@gmail.com>
2019-03-12 07:44:39 -0400

    implement is_empty? on heap type

--- lib/chapter_3/heap.ex
+++ lib/chapter_3/heap.ex
@@ -11,4 +11,8 @@ defmodule Heap do
 
   @spec empty() :: t(any())
   def empty, do: %__MODULE__{}
+
+  @spec is_empty?(t(any())) :: bool()
+  def is_empty?(%__MODULE__{node: :empty}), do: true
+  def is_empty?(_), do: false
 end
--- test/chapter_3/heap_test.exs
+++ test/chapter_3/heap_test.exs
@@ -6,4 +6,14 @@ defmodule HeapTest do
       assert %Heap{node: :empty, left: :empty, right: :empty} == Heap.empty()
     end
   end
+
+  describe "is_empty?/1" do
+    test "returns true if node is empty" do
+      assert Heap.empty() |> Heap.is_empty?()
+    end
+
+    test "returns false if node contains a value" do
+      refute %Heap{node: 1} |> Heap.is_empty?()
+    end
+  end
 end
