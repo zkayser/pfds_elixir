db78fe612b1e5a949ed4ced313cda4d7f9d96af9
Kayser <zkayser@gmail.com>
2019-03-25 10:23:26 -0400

    Add BinomialHeap implementation and tests

---
+++ lib/chapter_3/binomial_heap.ex
@@ -0,0 +1,118 @@
+defmodule BinomialHeap do
+  @moduledoc """
+  A binomial heap is a collection of heap-ordered binomial trees in which
+  no two trees have the same rank. This collection is represented as a list
+  of trees in increasing order of rank.
+
+  Because each binomial tree contains 2^r elements and no two trees have the
+  same rank, the trees in a binomial heap of size _n_ correspond exactly to the
+  ones in the binary representation of of _n_. For example, the binary representation
+  of 21 is 10101 so a binomial heap of size 21 would contain one tree of rank 0, one
+  tree of rank 2, and one tree of rank 4 (of sizes 1, 4, and 16, respectively).
+
+  Note that, just as the binary representation of _n_ contains at most |log(n + 1)|
+  ones, a binomial heap of size _n_ contains at most |log(n + 1)| trees.
+  """
+
+  @type tree(a) :: %{rank: non_neg_integer(), element: a, children: list(tree(a))}
+
+  @type heap(a) :: list(tree(a))
+
+  @doc """
+  Link maintains heap order by always linking trees with larger roots
+  under trees with smaller roots.
+  """
+  @spec link(tree(any), tree(any)) :: tree(any)
+  def link(%{rank: rank, element: x, children: children} = t1, %{element: y} = t2) when x <= y do
+    %{t1 | rank: rank + 1, children: [t2 | children]}
+  end
+
+  def link(%{rank: rank} = t1, %{children: children} = t2) do
+    %{t2 | rank: rank + 1, children: [t1 | children]}
+  end
+
+  @doc """
+  Inserts a new element into the heap.
+
+  #------------------------------------------
+  The worst case is insertion into a heap of
+  size _n = 2^k - 1_, requiring a total of
+  k links and O(k) = O(log n) time.
+  #------------------------------------------
+  """
+  @spec insert(any, heap(any)) :: heap(any)
+  def insert(heap, value), do: insert_(%{rank: 0, element: value, children: []}, heap)
+  defp insert_(t, []), do: [t]
+
+  defp insert_(%{rank: rank} = t, [%{rank: rank_2} | _] = trees) when rank < rank_2 do
+    [t | trees]
+  end
+
+  defp insert_(t, [tree | smaller]) do
+    t
+    |> link(tree)
+    |> insert_(smaller)
+  end
+
+  @doc """
+  Merges two heaps together.
+  """
+  @spec merge(heap(any), heap(any)) :: heap(any)
+  def merge(heap, []), do: heap
+  def merge([], heap), do: heap
+
+  def merge([%{rank: rank} = t1 | smaller], [%{rank: rank_2} | _] = heap_2) when rank < rank_2 do
+    [t1 | merge(smaller, heap_2)]
+  end
+
+  def merge([%{rank: rank} | _] = heap_1, [%{rank: rank_2} = t2 | smaller]) when rank_2 < rank do
+    [t2 | merge(heap_1, smaller)]
+  end
+
+  def merge([t1 | children_1], [t2 | children_2]) do
+    t1
+    |> link(t2)
+    |> insert_(merge(children_1, children_2))
+  end
+
+  @doc """
+  Returns the minimum value in the heap
+  """
+  def find_min(heap) do
+    {min, _} = remove_min_tree(heap)
+    min.element
+  end
+
+  @doc """
+  Deletes the minimum element from the heap.
+
+  #------------------------------------------
+  This is done by returning the children of
+  the discarded element to the remaining list
+  of binomial trees in the heap. Each list of
+  children is _almost_ a valid binomial heap
+  in that each is a collection of heap-ordered
+  binomial trees of unique rank, but in
+  decreasing rather than increasing order of
+  rank. We convert the list of children into
+  a valid binomial heap by reversing it and
+  then merging it with the remaining trees.
+  #------------------------------------------
+  """
+  @spec delete_min(heap(any)) :: heap(any)
+  def delete_min(heap) do
+    {%{children: children}, remaining} = remove_min_tree(heap)
+    merge(Enum.reverse(children), remaining)
+  end
+
+  defp remove_min_tree([tree]), do: {tree, []}
+
+  defp remove_min_tree([head | tail]) do
+    {min_element, min_tree} = remove_min_tree(tail)
+
+    case head.element > min_element.element do
+      true -> {min_element, [head | min_tree]}
+      false -> {head, tail}
+    end
+  end
+end
---
+++ test/chapter_3/binomial_heap_test.exs
@@ -0,0 +1,112 @@
+defmodule BinomialHeapTest do
+  use ExUnit.Case
+
+  describe "link/2" do
+    test "creates a binomial tree in heap-sorted order from two binomial trees" do
+      tree_1 = %{element: 2, children: [], rank: 0}
+      tree_2 = %{element: 5, children: [], rank: 0}
+
+      result = BinomialHeap.link(tree_1, tree_2)
+
+      assert result.element == 2
+      assert result.rank == 1
+      assert length(result.children) == 1
+    end
+
+    test "links two binomial trees with differing ranks" do
+      rank_one =
+        BinomialHeap.link(%{element: 2, children: [], rank: 0}, %{
+          element: 5,
+          children: [],
+          rank: 0
+        })
+
+      result = BinomialHeap.link(rank_one, %{element: 3, children: [], rank: 0})
+
+      assert result.element == 2
+      assert result.rank == 2
+      assert length(result.children) == 2
+    end
+  end
+
+  describe "insert/2" do
+    test "places a single element into an empty heap as a binomial tree of rank 0" do
+      assert BinomialHeap.insert([], 2) == [%{children: [], element: 2, rank: 0}]
+    end
+
+    test "places elements into the heap" do
+      heap =
+        []
+        |> BinomialHeap.insert(2)
+        |> BinomialHeap.insert(25)
+        |> BinomialHeap.insert(50)
+        |> BinomialHeap.insert(75)
+        |> BinomialHeap.insert(100)
+        |> BinomialHeap.insert(15)
+
+      assert length(heap) == 2
+      assert [head | tail] = heap
+      assert [last | _] = tail
+      assert head.rank == 1
+      assert last.rank == 2
+    end
+  end
+
+  describe "merge/2" do
+    test "returns the non-empty heap when given an empty and non-empty heap" do
+      heap = [%{element: 2, rank: 0, children: []}]
+      assert BinomialHeap.merge(heap, []) == heap
+      assert BinomialHeap.merge([], heap) == heap
+    end
+
+    test "creates a new heap from the two heaps passed in" do
+      heap_1 = BinomialHeap.insert([], 0) |> BinomialHeap.insert(25)
+      heap_2 = BinomialHeap.insert([], 10) |> BinomialHeap.insert(50)
+
+      expected = [
+        %{
+          children: [
+            %{children: [%{children: [], element: 50, rank: 0}], element: 10, rank: 1},
+            %{children: [], element: 25, rank: 0}
+          ],
+          element: 0,
+          rank: 2
+        }
+      ]
+
+      assert BinomialHeap.merge(heap_1, heap_2) == expected
+    end
+  end
+
+  describe "find_min/1" do
+    test "returns the minimum value from the heap" do
+      heap =
+        BinomialHeap.insert([], 100)
+        |> BinomialHeap.insert(20)
+        |> BinomialHeap.insert(10)
+        |> BinomialHeap.insert(50)
+
+      assert BinomialHeap.find_min(heap) == 10
+    end
+  end
+
+  describe "delete_min/1" do
+    test "removes the minimum value from the heap and returns a new heap" do
+      heap =
+        BinomialHeap.insert([], 100)
+        |> BinomialHeap.insert(20)
+        |> BinomialHeap.insert(10)
+        |> BinomialHeap.insert(50)
+
+      expected = [
+        %{children: [], element: 50, rank: 0},
+        %{children: [%{children: [], element: 100, rank: 0}], element: 20, rank: 1}
+      ]
+
+      result = BinomialHeap.delete_min(heap)
+
+      assert result == expected
+      refute hd(result).element == 10
+    end
+  end
+end
