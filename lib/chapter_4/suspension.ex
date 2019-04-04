6e4e438f95a7b49a355ce153e922098607f30b43
Kayser <zkayser@gmail.com>
2019-04-04 08:14:32 -0400

    Rename Suspension.suspend/1 to Suspension.create/1

--- lib/chapter_4/suspension.ex
+++ lib/chapter_4/suspension.ex
@@ -4,8 +4,8 @@ defmodule Suspension do
 
   defstruct fun: nil
 
-  def suspend({mod, fun, args}), do: %Suspension{fun: fn -> apply(mod, fun, args) end}
-  def suspend(expression), do: %Suspension{fun: fn -> expression end}
+  def create({mod, fun, args}), do: %Suspension{fun: fn -> apply(mod, fun, args) end}
+  def create(expression), do: %Suspension{fun: fn -> expression end}
 
   def force(%Suspension{fun: susp}) do
     susp.()
--- test/chapter_4/suspension_test.exs
+++ test/chapter_4/suspension_test.exs
@@ -1,22 +1,22 @@
 defmodule SuspensionTest do
   use ExUnit.Case
 
-  describe "suspend/1" do
+  describe "create/1" do
     test "wraps a value inside of a function" do
-      fun = Suspension.suspend(5) |> Map.get(:fun)
+      fun = Suspension.create(5) |> Map.get(:fun)
       assert fun.() == 5
     end
 
     test "creates a suspension from tuples with mod, fun, args" do
-      fun = Suspension.suspend({Kernel, :+, [1, 2]}) |> Map.get(:fun)
+      fun = Suspension.create({Kernel, :+, [1, 2]}) |> Map.get(:fun)
       assert fun.() == 3
     end
   end
 
   describe "force/1" do
     test "calls the suspended function" do
-      assert 5 == Suspension.suspend(5) |> Suspension.force()
-      assert 5 == Suspension.suspend({Kernel, :+, [2, 3]}) |> Suspension.force()
+      assert 5 == Suspension.create(5) |> Suspension.force()
+      assert 5 == Suspension.create({Kernel, :+, [2, 3]}) |> Suspension.force()
     end
   end
 end
