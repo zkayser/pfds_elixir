eaba1a09c77b9258926c9edbb49a8c990ef41582
Kayser <zkayser@gmail.com>
2019-04-03 21:02:04 -0400

    Add {module, fun, args} pattern match for Suspension.suspend/1

--- lib/chapter_4/suspension.ex
+++ lib/chapter_4/suspension.ex
@@ -4,6 +4,7 @@ defmodule Suspension do
 
   defstruct fun: nil
 
+  def suspend({mod, fun, args}), do: %Suspension{fun: fn -> apply(mod, fun, args) end}
   def suspend(expression), do: %Suspension{fun: fn -> expression end}
 
   def force(%Suspension{fun: susp}) do
--- test/chapter_4/suspension_test.exs
+++ test/chapter_4/suspension_test.exs
@@ -6,6 +6,11 @@ defmodule SuspensionTest do
       fun = Suspension.suspend(5) |> Map.get(:fun)
       assert fun.() == 5
     end
+
+    test "creates a suspension from tuples with mod, fun, args" do
+      fun = Suspension.suspend({Kernel, :+, [1, 2]}) |> Map.get(:fun)
+      assert fun.() == 3
+    end
   end
 
   describe "force/1" do
