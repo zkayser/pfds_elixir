7f19895d849584e0be9f7d7287461190116c819d
Kayser <zkayser@gmail.com>
2019-04-24 08:15:48 -0400

    Add link/2 function to combine trees

--- lib/chapter_6/lazy_binomial_heap.ex
+++ lib/chapter_6/lazy_binomial_heap.ex
@@ -18,4 +18,16 @@ defmodule LazyBinomialHeap do
       _ -> false
     end
   end
+
+  @doc """
+  Links two trees together, placing the tree with the larger
+  root as the leftmost child of the tree with the smaller root.
+  """
+  @spec link(tree(any), tree(any)) :: tree(any)
+  def link(%{element: x, children: c1} = tree_1, %{element: y, children: c2} = tree_2) do
+    case x <= y do
+      true -> %{tree_1 | rank: tree_1.rank + 1, children: [tree_2 | c1]}
+      _ -> %{tree_2 | rank: tree_1.rank + 1, children: [tree_1 | c2]}
+    end
+  end
 end
--- test/chapter_6/lazy_binomial_heap_test.exs
+++ test/chapter_6/lazy_binomial_heap_test.exs
@@ -13,4 +13,12 @@ defmodule LazyBinomialHeapTest do
       assert Heap.empty?(Heap.empty())
     end
   end
+
+  describe "link/2" do
+    test "merges two trees together by making the tree with the larger root the leftmost child of the other tree" do
+      tree_1 = %{rank: 1, element: 1, children: []}
+      tree_2 = %{rank: 1, element: 2, children: []}
+      assert Heap.link(tree_1, tree_2) == %{tree_1 | rank: 2, children: [tree_2]}
+    end
+  end
 end
