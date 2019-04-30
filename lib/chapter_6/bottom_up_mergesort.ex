084494552fff78b3bdad60f0e19d95b3449c2cf2
Kayser <zkayser@gmail.com>
2019-04-30 08:58:50 -0400

    Implement add/2 function for BottomUpMergeSort

--- lib/chapter_6/bottom_up_mergesort.ex
+++ lib/chapter_6/bottom_up_mergesort.ex
@@ -35,4 +35,27 @@ defmodule BottomUpMergeSort do
   def mrg(list, []), do: list
   def mrg([x | xs_], [y | _] = ys) when x <= y, do: [x | mrg(xs_, ys)]
   def mrg(xs, [y | ys_]), do: [y | mrg(xs, ys_)]
+
+  @doc """
+  Lazily adds a new element to the sortable collection.
+  """
+  @spec add(t(any), any) :: t(any)
+  def add(%__MODULE__{} = collection, element) do
+    %__MODULE__{
+      size: collection.size + 1,
+      segments:
+        Suspension.create(__MODULE__, :add_segment, [
+          [element],
+          Suspension.force(collection.segments),
+          collection.size
+        ])
+    }
+  end
+
+  @spec add_segment(list(any), list(any), non_neg_integer) :: list(any)
+  def add_segment(seg, segs, size) when rem(size, 2) == 0, do: [seg | segs]
+
+  def add_segment(seg, [head_seg | tail], size) do
+    add_segment(mrg(seg, head_seg), tail, div(size, 2))
+  end
 end
--- test/chapter_6/bottom_up_mergesort_test.exs
+++ test/chapter_6/bottom_up_mergesort_test.exs
@@ -22,4 +22,32 @@ defmodule BottomUpMergeSortTest do
       assert Sortable.mrg(first, second) == [1, 2, 3, 4, 5, 6]
     end
   end
+
+  describe "add/2" do
+    test "adds an element into the collection" do
+      segments = Sortable.init() |> Sortable.add(1) |> Map.get(:segments) |> Suspension.force()
+
+      assert segments == [[1]]
+
+      new_segments =
+        Sortable.add(%Sortable{segments: Suspension.create(segments), size: 1}, 7)
+        |> Sortable.add(4)
+        |> Sortable.add(5)
+        |> Map.get(:segments)
+        |> Suspension.force()
+
+      Enum.reduce(:lists.flatten(new_segments), 0, fn x, acc ->
+        assert acc <= x
+        x
+      end)
+    end
+
+    test "increments the size property on the collection" do
+      collection = Sortable.init()
+      assert collection.size == 0
+
+      new_collection = Sortable.add(collection, 1)
+      assert new_collection.size == 1
+    end
+  end
 end
