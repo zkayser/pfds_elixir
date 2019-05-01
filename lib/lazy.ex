552cb0923584d4475a41b6bfc9c9565c96c7cbb5
Kayser <zkayser@gmail.com>
2019-05-01 07:26:56 -0400

    Refactor LazyAstGenerators module to expose only one public function

--- lib/lazy.ex
+++ lib/lazy.ex
@@ -2,11 +2,12 @@ defmodule Lazy do
   alias LazyAstGenerators, as: Generators
 
   defmacro deflazy(head, body) do
-    {func_name, args_ast} = Generators.name_and_args(head)
-    args = Generators.get_args(args_ast)
-    impl_body_ast = Generators.impl_body_ast(body, args)
-    lazy_impl_ast = Generators.lazy_impl_ast(head, func_name)
-    {impl_func_name, _} = Generators.name_and_args(lazy_impl_ast)
+    %{
+      impl_func_name: impl_func_name,
+      original_args_ast: args_ast,
+      lazy_impl_ast: lazy_impl_ast,
+      impl_body_ast: impl_body_ast
+    } = Generators.build_representations(head, body)
 
     quote do
       def unquote(head) do
@@ -27,7 +28,23 @@ defmodule LazyAstGenerators do
   Exposes utility functions for generating ASTs used
   in the `deflazy` macro
   """
-  def lazy_impl_ast(head, func_name) do
+
+  def build_representations(head, body) do
+    {func_name, args_ast} = name_and_args(head)
+    args = get_args(args_ast)
+    impl_body_ast = impl_body_ast(body, args)
+    lazy_impl_ast = lazy_impl_ast(head, func_name)
+    {impl_func_name, _} = name_and_args(lazy_impl_ast)
+
+    %{
+      original_args_ast: args_ast,
+      impl_func_name: impl_func_name,
+      lazy_impl_ast: lazy_impl_ast,
+      impl_body_ast: impl_body_ast
+    }
+  end
+
+  defp lazy_impl_ast(head, func_name) do
     Macro.postwalk(head, fn
       {func, context, args} when func == func_name ->
         {:"#{func_name}_lazy_implementation", context, args}
@@ -37,14 +54,14 @@ defmodule LazyAstGenerators do
     end)
   end
 
-  def impl_body_ast(body, args),
+  defp impl_body_ast(body, args),
     do: Macro.postwalk(body[:do], fn node -> force_suspensions(node, args) end)
 
-  def name_and_args({:when, _, [short_head | _]}) do
+  defp name_and_args({:when, _, [short_head | _]}) do
     name_and_args(short_head)
   end
 
-  def name_and_args(short_head) do
+  defp name_and_args(short_head) do
     Macro.decompose_call(short_head)
   end
 
@@ -60,12 +77,12 @@ defmodule LazyAstGenerators do
     end
   end
 
-  def get_args(args_ast) when is_list(args_ast) do
+  defp get_args(args_ast) when is_list(args_ast) do
     Enum.reduce(args_ast, [], &get_arg/2)
   end
 
-  def get_arg({arg, _context, nil}, arg_list), do: [arg | arg_list]
-  def get_arg(_primitive, arg_list), do: arg_list
+  defp get_arg({arg, _context, nil}, arg_list), do: [arg | arg_list]
+  defp get_arg(_primitive, arg_list), do: arg_list
 
   defp force_suspensions(node, _), do: node
 end
