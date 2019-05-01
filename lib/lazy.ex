2bffb37fdeaec8c6cf5479d7b35e5079134e2c2c
Kayser <zkayser@gmail.com>
2019-05-01 07:12:45 -0400

    Refactor utility functions in Lazy module to separate module to prevent unneeded imports to Caller modules

--- lib/lazy.ex
+++ lib/lazy.ex
@@ -1,39 +1,50 @@
 defmodule Lazy do
+  alias LazyAstGenerators, as: Generators
+
   defmacro deflazy(head, body) do
-    {func_name, args_ast} = name_and_args(head)
-    args = get_args(args_ast)
-
-    ast = Macro.postwalk(body[:do], fn node -> force_suspensions(node, args) end)
-
-    actual_work_ast =
-      Macro.postwalk(head, fn
-        {func, context, args} when func == func_name ->
-          {:"#{func_name}_lazy_implementation", context, args}
-
-        node ->
-          node
-      end)
-
-    {worker_func, _} = name_and_args(actual_work_ast)
+    {func_name, args_ast} = Generators.name_and_args(head)
+    args = Generators.get_args(args_ast)
+    impl_body_ast = Generators.impl_body_ast(body, args)
+    lazy_impl_ast = Generators.lazy_impl_ast(head, func_name)
+    {impl_func_name, _} = Generators.name_and_args(lazy_impl_ast)
 
     quote do
       def unquote(head) do
-        Suspension.create(__MODULE__, unquote(worker_func), unquote(args_ast))
+        Suspension.create(__MODULE__, unquote(impl_func_name), unquote(args_ast))
       end
 
-      def unquote(actual_work_ast) do
-        unquote(ast)
+      def unquote(lazy_impl_ast) do
+        unquote(impl_body_ast)
       end
     end
   end
 
   def eval(arg), do: Suspension.force(arg)
+end
 
-  defp name_and_args({:when, _, [short_head | _]}) do
+defmodule LazyAstGenerators do
+  @moduledoc """
+  Exposes utility functions for generating ASTs used
+  in the `deflazy` macro
+  """
+  def lazy_impl_ast(head, func_name) do
+    Macro.postwalk(head, fn
+      {func, context, args} when func == func_name ->
+        {:"#{func_name}_lazy_implementation", context, args}
+
+      node ->
+        node
+    end)
+  end
+
+  def impl_body_ast(body, args),
+    do: Macro.postwalk(body[:do], fn node -> force_suspensions(node, args) end)
+
+  def name_and_args({:when, _, [short_head | _]}) do
     name_and_args(short_head)
   end
 
-  defp name_and_args(short_head) do
+  def name_and_args(short_head) do
     Macro.decompose_call(short_head)
   end
 
@@ -49,12 +60,12 @@ defmodule Lazy do
     end
   end
 
-  defp force_suspensions(node, _), do: node
-
-  defp get_args(args_ast) when is_list(args_ast) do
+  def get_args(args_ast) when is_list(args_ast) do
     Enum.reduce(args_ast, [], &get_arg/2)
   end
 
-  defp get_arg({arg, _context, nil}, arg_list), do: [arg | arg_list]
-  defp get_arg(_primitive, arg_list), do: arg_list
+  def get_arg({arg, _context, nil}, arg_list), do: [arg | arg_list]
+  def get_arg(_primitive, arg_list), do: arg_list
+
+  defp force_suspensions(node, _), do: node
 end
